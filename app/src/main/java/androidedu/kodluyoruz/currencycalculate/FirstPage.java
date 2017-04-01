78f88abc1ad4a75fe9ffc174ba45bb1645b50ae6
Rozerin Köysu <rozerinkoysu@gmail.com>
2017-04-01 16:36:34 +0300

    dsf

--- app/src/main/java/androidedu/kodluyoruz/currencycalculate/FirstPage.java
+++ app/src/main/java/androidedu/kodluyoruz/currencycalculate/FirstPage.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.AppCompatButton;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -19,22 +20,50 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 import static androidedu.kodluyoruz.currencycalculate.R.id.spinner;  // doğru bir kullanım mı ?
 
-public class FirstPage extends AppCompatActivity implements View.OnClickListener, TextWatcher {
+public class FirstPage extends AppCompatActivity implements View.OnClickListener, TextWatcher, AdapterView.OnItemSelectedListener {
 
-    public String islem_tipi;
-    public int alis_degeri;
-    public int satis_degeri;
-    public String doviz_ismi;
+    public boolean islem_tipi;
+    public double alis_degeri;
+    public double satis_degeri;
+    private String doviz_turu;
     //
-    EditText tlSigned;
-    EditText tlDecimal;
-    EditText dovizSigned;
-    EditText dovizDecimal;
+    private EditText tlSigned = null;
+    private EditText tlDecimal;
+    private EditText dovizSigned;
+    private EditText dovizDecimal;
+    private TextView dovizIsmi;
+    private Button btnChangeCurrency;
+    private Spinner spinner;
+
+    public double getAlis_degeri() {
+        return alis_degeri;
+    }
+
+    public void setAlis_degeri(double alis_degeri) {
+        this.alis_degeri = alis_degeri;
+    }
+
+    public double getSatis_degeri() {
+        return satis_degeri;
+    }
+
+    public void setSatis_degeri(double satis_degeri) {
+        this.satis_degeri = satis_degeri;
+    }
+
+    public String getDoviz_turu() {
+        return doviz_turu;
+    }
+
+    public void setDoviz_turu(String doviz_turu) {
+        this.doviz_turu = doviz_turu;
+    }
 //    public int tl_signed_alan;
 //    public int tl_decimal_alan;
 
@@ -43,115 +72,37 @@ public class FirstPage extends AppCompatActivity implements View.OnClickListener
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
         initView();
+        initEvent();
     }
 
 
     public void initView() {
 
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner = (Spinner) findViewById(R.id.spinner);
 
-        TextView tlTutarı = (TextView) findViewById(R.id.activity_first_page_tlTutarı);
+
         tlSigned = (EditText) findViewById(R.id.activity_first_page_tlSigned);
-        TextView comma1 = (TextView) findViewById(R.id.activity_first_page_comma1);
         tlDecimal = (EditText) findViewById(R.id.activity_first_page__tlDecimal);
-        TextView tlIsmi = (TextView) findViewById(R.id.activity_first_page_TL);
-
-        TextView dovizTutarı = (TextView) findViewById(R.id.activity_first_page_dovizTutarı);
         dovizSigned = (EditText) findViewById(R.id.activity_first_page_dovizSigned);
-        TextView comma = (TextView) findViewById(R.id.activity_first_page_comma);
         dovizDecimal = (EditText) findViewById(R.id.activity_first_page_dovizDecimal);
-        TextView dovizIsmi = (TextView) findViewById(R.id.activity_first_page_dovizIsmi);
-
-
-//        tl_signed_alan = Integer.parseInt(tlSigned.getText().toString());
-//        tl_decimal_alan = Integer.parseInt(tlDecimal.getText().toString());
-        Button btnChangeCurrency = (Button) findViewById(R.id.activity_first_page_buttonchangecurrency);
-        btnChangeCurrency.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(FirstPage.this, SecondPage.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
+        dovizIsmi = (TextView) findViewById(R.id.activity_first_page_dovizIsmi);
+        btnChangeCurrency = (Button) findViewById(R.id.activity_first_page_buttonchangecurrency);
 
 
     }
 
-    public void initEvent(final Spinner spinner) {
-        //passNext.setOnClickListener(this);
+    public void initEvent() {
 
-
-//        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//
-//               // islem_tipi = spinner.getSelectedItem().toString();
-////                // diğer sayfadan aldığın döviz cinsini doviz_ismi değişkenne burada ata
-//                if (islem_tipi == "Alış") { // parent.getItemAtPosition(position).toString()
-//                    AlisHesapla(alis_degeri);
-//                } else if (islem_tipi == "Satış") {
-//                    SatisHesapla(satis_degeri);
-//                }
-//            }
-//
-//            @Override
-//            public void onNothingSelected(AdapterView<?> parent) {
-//
-//            }
-//
-//
-//  });
-        tlSigned.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                islem_tipi = spinner.getSelectedItem().toString();
-                Intent myIntent = FirstPage.this.getIntent();
-                alis_degeri = myIntent.getExtras().getInt("alis");
-                satis_degeri = myIntent.getExtras().getInt("satis");
-                doviz_ismi = myIntent.getExtras().getString("dovizismi");
-
-                if (Integer.parseInt(s.toString()) != 00) {
-                    if (islem_tipi == "Alış") { // parent.getItemAtPosition(position).toString()
-                        AlisHesapla(alis_degeri);
-                    } else if (islem_tipi == "Satış") {
-                        SatisHesapla(satis_degeri);
-                    }
-
-                }
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-
-
-        });
-    }
-
-
-    public void AlisHesapla(int alis_degeri) {
-        int x = Integer.parseInt(tlSigned.getText().toString());
-        dovizSigned.setText(x * alis_degeri);
-
+        btnChangeCurrency.setOnClickListener(this);
+        tlSigned.addTextChangedListener(this);
+        tlDecimal.addTextChangedListener(this);
+        ArrayAdapter spinnerAdapter = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
+        spinner.setOnItemSelectedListener(this);
 
     }
 
-    public void SatisHesapla(int satis_degeri) {
-        int x = Integer.parseInt(tlSigned.getText().toString());
-        dovizSigned.setText(x * satis_degeri);
-
-    }
 
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -161,15 +112,82 @@ public class FirstPage extends AppCompatActivity implements View.OnClickListener
     @Override
     public void onTextChanged(CharSequence s, int start, int before, int count) {
 
+        Log.e(tlSigned.getText().toString(), "onTextChanged: ");
+        if (String.valueOf(tlSigned.getText()).trim().startsWith("0")) {
+            if (String.valueOf(tlSigned.getText()).trim().length() == 1) {
+                tlSigned.setText("");
+            } else {
+                tlSigned.setText(tlSigned.getText().subSequence(1, tlSigned.getText().length()));
+            }
+
+        }
+
+        CalculateCurrency();
+
     }
+
+
+    public void CalculateCurrency() {
+        double totalAmount = Double.valueOf(String.valueOf(tlSigned.getText()) + "." + String.valueOf(tlDecimal.getText()));
+        if (islem_tipi == true) {
+            dovizSigned.setText(String.valueOf(totalAmount * satis_degeri).split("//.")[0]);
+            dovizDecimal.setText(String.valueOf(totalAmount * satis_degeri).split("//.")[1]);
+        } else {
+            dovizSigned.setText(String.valueOf(totalAmount * alis_degeri).split("//.")[0]);
+            dovizDecimal.setText(String.valueOf(totalAmount * alis_degeri).split("//.")[1]);
+        }
+            }
 
     @Override
     public void afterTextChanged(Editable s) {
+//        if(s.hashCode() == tlSigned.getText().hashCode()){ // burada iki adet edittexttehn hangisinin değiştiğini kontrol edebilirsin
+//
+//        }
+//        else(s.hashCode() == tlDecimal.getText().hashCode()){
+//
+//        }
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        Intent intent = new Intent(FirstPage.this, SecondPage.class);
+        startActivity(intent);
+        finish();
+
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent myIntent = (FirstPage.this).getIntent();
+
+//        alis_degeri = myIntent.getExtras().getString("alis");
+//        satis_degeri = myIntent.getExtras().getString("satis");
+        //  String s = myIntent.getExtras().getString("doviz");
+        //  dovizIsmi.setText(s);
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        islem_tipi = position == 0;
+        // CalculateCurrency();
+//
+//
+//        if (islem_tipi == "Alış") { // parent.getItemAtPosition(position).toString() // böyle yapma çünkü String değerleri
+//            AlisHesapla(alis_degeri);                                   // değiştirmek istediğinde bu alanları edit etmen zor olacaktır.
+//        } else if (islem_tipi == "Satış") {
+//            SatisHesapla(satis_degeri);
+//        }
 
     }
 
     @Override
-    public void onClick(View v) {
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
